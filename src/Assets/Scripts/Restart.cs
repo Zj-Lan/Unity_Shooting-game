b51d572d4d4adeba0763337fd03d7eef4bbee133
Lanzzzzz <lanzj_mail@qq.com>
2020-10-01 15:43:21 -0230

    Add files via upload

---
+++ src/Assets/Scripts/Restart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    // respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the RestartGame function in the game manager
+			GameManager.gm.RestartGame();
+		}
+	}
+}
